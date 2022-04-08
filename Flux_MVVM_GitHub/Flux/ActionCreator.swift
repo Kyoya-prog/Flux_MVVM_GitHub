f4772b73c5a44e949eaac6845b3d17bef3980e02
kyoya <kyoyaprog@gmail.com>
2022-04-08 16:58:48 +0900

    ActionCreatorの実装

---
+++ Flux_MVVM_GitHub/Flux/ActionCreator.swift
@@ -0,0 +1,33 @@
+//
+//  ActionCreator.swift
+//  Flux_MVVM_GitHub
+//
+//  Created by 松山響也 on 2022/04/08.
+//
+
+import Dispatch
+
+final class ActionCreator{
+    private let dispatcher:Dispatcher
+    private let apiSession:GitHubApiRequestable
+    
+    init(dispatcher: Dispatcher = .shared, apiSession:GitHubApiSession = GitHubApiSession.shared){
+        self.dispatcher = dispatcher
+        self.apiSession = apiSession
+    }
+    
+    func searchRepositories(query:String,page:Int){
+        apiSession.searchRepositories(query: query, page: page) {[weak self] result in
+            switch result {
+            case let .success((repositories, _)):
+                self?.dispatcher.dispatch(.addRepositories(repositories))
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+    
+    func clearRepositories(){
+        dispatcher.dispatch(.clearRepositories)
+    }
+}
